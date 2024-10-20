213f2515e7a2408ff07f99a642ff5850fdbf7a78
Gary Magill <123419905+GaryMagill22@users.noreply.github.com>
2024-10-20 09:26:21 -0600

    commit

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,7 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      'react-native-paper/babel',
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-      }],
+      'react-native-paper/babel'
     ],
   };
 };
